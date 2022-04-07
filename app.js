fdb22810f62e66132b0863beefa33e5a8cb787f7
Muhammad Jafar <muhammadjafarrahimjonzoda@gmail.com>
2022-04-08 01:36:03 +0500

    better js

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
-const main = document.querySelector(".main_content");
-const section = document.querySelectorAll(".section");
-const controls = document.querySelectorAll(".controls");
-const control = document.querySelectorAll(".control");
-    
-function PageTrans(){
-    for(i = 0; i < control.length; i++){
-        control[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll(".btn_active");
-            currentBtn[0].className = currentBtn[0].className.replace('btn_active', '');
-            this.className += ' btn_active';
-         })
-    }
-
-    main.addEventListener('click',(e)=>{
-        const id = e.target.dataset.id;
-        if(id){
-            controls.forEach(cont=>{
-                cont.classList.remove('active');
-            })
-            e.target.classList.add('active');
-
-            section.forEach(sec=>{
-                sec.classList.remove('active');
-            })
-           
-            const elem = document.getElementById(id);
-            elem.classList.add('active');
-        }
+(function(){
+    [...document.querySelectorAll(".control")].forEach(button=>{
+        button.addEventListener('click',function(){
+            document.querySelector('.btn_active').classList.remove('btn_active');
+            this.classList.add('btn_active');
+            document.querySelector('.active').classList.remove('active');
+            document.getElementById(button.dataset.id).classList.add('active'); 
+        })
     })
-}
-
-PageTrans();
+})();
